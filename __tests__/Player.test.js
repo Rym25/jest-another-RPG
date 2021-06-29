8e3a34c1b073a028b854ab4559e4210bec4e7bed
Ryely <ryely.moore@outlook.com>
2021-06-29 01:03:37 -0600

    added methods to the player function as prototypes.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,6 @@
 const Potion = require('../lib/Potion');
 const Player = require('../lib/Player');
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
 
 jest.mock('../lib/Potion');
 
@@ -14,4 +14,23 @@ test('creates a player object', () => {
     expect(player.inventory).toEqual(
         expect.arrayContaining([expect.any(Object)])
     );
+})
+
+test("get player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test("gets inventory from player or returns false", () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 })
