e319d1066391b4bf32bfb9f488ae2b538a69f813
Ryely <ryely.moore@outlook.com>
2021-06-28 23:36:53 -0600

    created the Potion constructor to either take in a specific potion or assign a randomly potion type to it.

--- _tests_/random.test.js
+++
@@ -1,6 +0,0 @@
-const randomNumber = require('../lib/random.js');
-
-test('checks if 1 is equal to 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
